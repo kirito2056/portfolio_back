875f689b95398ed17338c472834c084fc246489a
하진 권씨 <hajin_2056@naver.com>
2025-05-25 02:03:16 +0900

    CHORE :: 대충 구상?

--- src/main/java/hajin/xyz/portfolio/PortfolioApplication.java
+++ src/main/java/hajin/xyz/portfolio/PortfolioApplication.java
@@ -11,3 +11,19 @@ public class PortfolioApplication {
 	}
 
 }
+
+/*
+
+	사이드 프로젝트는 패키지별로 entity, repository, controller 관리
+	메인 repository 없음 General에 있는 Repo, Entity 삭제 예정
+	사실 그냥 DB 없이 대충 해도 되긴 함
+
+	sideproject package에 package 생성 후 프론트 작업
+	html은 api로 반환 + react는 front 깃헙 레포에서 관리
+	static에서 관리? 해야하나?
+	React HTML View -> XSS방지
+	Security - CORS + "/*"
+
+
+
+ */
