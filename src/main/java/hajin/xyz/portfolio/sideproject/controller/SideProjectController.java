5f7b71654107531815fd6bfc9e393bd046eec138
하진 권씨 <hajin_2056@naver.com>
2025-05-26 14:15:46 +0900

    CREATE :: 4번째 우려먹는 numberCLock

---
+++ src/main/java/hajin/xyz/portfolio/sideproject/controller/SideProjectController.java
@@ -0,0 +1,14 @@
+package hajin.xyz.portfolio.sideproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/side")
+public class SideProjectController {
+
+    @RequestMapping("/numberclock")
+    public String numberClock() {
+        return "numberClock/numberclock.html";
+    }
+}
